1e22843cee8f5b4f7b8b0dcd5767dbf9df53ecb1
opegol <opgbenga@gmail.com>
2023-09-29 00:57:00 +0000

    Edits to tasks 2 & 101 in 0x14 bit manipulation project

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,33 +7,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	int j, t = 0;
-	unsigned long int i;
-	unsigned long int num;
-	unsigned int mask;
+	unsigned long int mask = 1;
 
-	if (n == 0)
-	{
-		_putchar(n + '0');
-		return;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	n = n & mask;
+	_putchar(n + '0');
 
-
-	num = (8 * sizeof(int));
-	mask = 1 << (num - 1);
-
-	for (i = 0; i < num; i++)
-	{
-		j = (n & mask) ? 1 : 0;
-
-		if (j == 0 && t == 0)
-		{
-			mask >>= 1;
-			continue;
-		}
-		if (j == 1 && t == 0)
-			t = 1;
-		_putchar(j + '0');
-		mask >>= 1;
-	}
 }
