742a0c008fe39a2cf6717d3c7d14fabe1c4c4224
opegol <opgbenga@gmail.com>
2024-02-22 02:19:32 +0000

    First commit for 0x1C-makefiles project

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid
+        grid is a list of list of integers
+        0 represents a water zone
+        1 represents a land zone
+        One cell is a square with side length 1.
+
+    Args:
+        grid (list): list of lists representing a rectangular block.
+
+    Returns:
+        perimeter of the connected land.
+    """
+
+    x = len(grid[0])
+    y = len(grid)
+    perim = 0
+    for i in range(y-2):
+        for j in range(x-2):
+            if grid[i+1][j+1] == 1:
+                zeros = (4 - (grid[i][j+1] + grid[i+2][j+1]
+                         + grid[i+1][j] + grid[i+1][j+2]))
+                if zeros < 4:
+                    perim += zeros
+    return perim
